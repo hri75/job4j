828664b954a902dd3f10cdf27d7b10d5ed95fb3d
Rustam Harisov <hri75@mail.ru>
2018-04-17 14:42:20 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+ * Class Класс для вычисления арифметических операций
+ * @author rustam.harisov
+ * @since 17.04.2018
+ * @version 1.0
+ */
+
 public class Calculate{
+	
+	/** main - точка входа в программу(точка запуска)
+	 *  выводит строку в консоль
+	 * @params arg - в arg будут переданы аргументы из командой строки  
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
 	}
