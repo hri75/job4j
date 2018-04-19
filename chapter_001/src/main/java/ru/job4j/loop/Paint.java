6f078d8faa36fb17a0c4a89a6cdad3b9f29ee35e
Rustam Harisov <hri75@mail.ru>
2018-04-19 17:51:07 +0300

    4.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Class Paint - рисуем пирамиду в псевдографике.
  */
@@ -11,26 +13,58 @@ public class Paint {
      * @return - строка, содержащая пирамиду (с разделителями строк).
      */
     public String pyramid(int height) {
-        // Буфер для результата.
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> column >= height - row - 1 && column <= height + row - 1
+        );
+    }
+
+    /**
+     * Правая половинка пирамиды.
+     * @param height - высота пирамиды
+     * @return - строка, содержащая правую половинку пирамиды.
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    /**
+     * Левая половинка пирамиды.
+     * @param height - высота пирамиды.
+     * @return - строка, содержащая левую половинку пирамиды.
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
+    /**
+     * Универсальная функция.
+     * @param height
+     * @param weight
+     * @param predict
+     * @return
+     */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        // ширина будет равна высоте.
-        int weight = 2 * height - 1;
-        // внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
-            // внутренний цикл определяет положение ячейки в строке.
             for (int column = 0; column != weight; column++) {
-                // если строка равна ячейки, то рисуем галку.
-                // в данном случае строка определяем, сколько галок будет на строке
-                if (column >= height - row - 1 && column <= height + row - 1) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
                 }
             }
-            // добавляем перевод строки.
             screen.append(System.lineSeparator());
         }
-        // Получаем результат.
         return screen.toString();
     }
 }
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -46,4 +46,45 @@ public class PaintTest {
                 )
         );
     }
+
+    /**
+     * Test для правой половинки пирамиды.
+     */
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Test для левой половинки пирамиды.
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
 }
