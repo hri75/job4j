68f24b69db38e60a5cb46a8bc7073ad75a06efd3
Rustam Harisov <hri75@mail.ru>
2018-04-23 13:31:53 +0300

    5.6. Удаление дубликатов в массиве. [#225] - убрал пустые строки и комментарии.

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -13,22 +13,18 @@ public class ArrayDuplicate {
      * @return - массив без дублирующихся слов.
      */
     public String[] remove(String[] array) {
-        int duplic = 0; //количество повторяющихся слов.
-        String temp; //временная переменная, пригодится для "сдвига".
-
-        // во внешнем цикле нет смысла идти до самого последнего уникального элемента, т.к. надо будет сравнивать его с n+1 элементом, поэтому -1:
+        int duplic = 0;
+        String temp;
         for (int i = 0; i < array.length - duplic - 1; i++) {
-            //во внутреннем цикле идем от n+1 до последнего уникального элемента:
             for (int j = i + 1; j < array.length - duplic; j++) {
                 if (array[i].equals(array[j])) {
                     duplic++;
-                    //сделать сдвиг: кинуть повторяющееся слово в конец, а все остальные сдвинуть влево на место "кинутого" слова.
                     temp = array[j];
                     for (int k = j; k < array.length - 1; k++) {
                         array[k] = array[k + 1];
                     }
                     array[array.length - 1] = temp;
-                    j--; //надо снова проверить элемент с j-м индексом, вдруг там опять повторяющееся слово.
+                    j--;
                 }
             }
         }
