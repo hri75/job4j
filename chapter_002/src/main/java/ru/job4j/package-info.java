4a1bc597717609ba2fbc5abb32bccc359ebaabca
Rustam Harisov <hri75@mail.ru>
2018-05-15 16:41:22 +0300

    2. Travis CI #31727

---
+++ chapter_002/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/** 
+ * Package for Test
+ * @author Rustam Harisov (hri75@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+ 
+ package ru.job4j;
---
+++ chapter_x/src/main/java/ru/job4j/Prosto.java
@@ -0,0 +1,5 @@
+public class Prosto {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
